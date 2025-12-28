66ea3e912c2fabf75077cd209c64d93bcebcdce0
skellent <skellent69@gmail.com>
2025-12-27 23:13:16 -0400

    main.py refactorizado y archivos de docs creados

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# Skell's Maretu
+# Skell's Maretu "LITE"
+
+!["icono"](./img/icon.png)
+
+![Static Badge](https://badgers.space/badge/python/+3.14/blue)
+![Static Badge](https://badgers.space/badge/library/Flet/pink)
+![Static Badge](https://badgers.space/badge/database/SQLite3/green)
+![Static Badge](https://badgers.space/github/license/Skell-s-Software/Maretu)
+![Static Badge](https://badgers.space/github/release/skellent/Maretu-Lite)
+
+## ¿Qué es Skell's Maretu "Lite" (Maretu)?
 
 Maretu (o Skell's Maretu "Lite") es una aplicación de punto de venta para dispositivos Android y próximamente ejecutable en Windows; funcionando completamente de manera local sin dependencias al internet.
+
+## ¿Para qué sirve Maretu?
+
+placeholder
+
+## ¿Maretu es seguro?
+
+placeholder  (si)
+
+## ¿Dónde puedo descargarlo?
+
+placeholder (link a releases y sitio web)
--- src/main.py
+++ src/main.py
@@ -1,80 +1,72 @@
+# Importacion de Flet
 import flet as ft
-from views.principal     import principal
-from views.ventas        import ventas
-from views.reportes      import reportes
-from views.inventario    import inventario
-from views.clientes      import clientes
-from views.configuracion import configuracion
-from typing              import Any
+# Importacion de Vistas
+from views import principal
+# Importacion de Modulos
 
-def main(page: ft.Page) -> Any:
-    def cambiarColor() -> ft.Colors:
-        color: str|None = page.client_storage.get("color")
-        if color == None:
-            return ft.Colors.DEEP_PURPLE
-        else:
-            match color:
-                case "purple":
-                    return ft.Colors.DEEP_PURPLE
-                case "green":
-                    return ft.Colors.GREEN
-                case "blue":
-                    return ft.Colors.BLUE
-                case "red":
-                    return ft.Colors.RED
-    def cambiarTema() -> ft.ThemeMode:
-        tema: str|None = page.client_storage.get("tema")
-        if tema == None:
-            if page.platform_brightness == ft.Brightness.LIGHT:
-                return ft.ThemeMode.LIGHT 
-            else:
+# Clase Primaria
+class Primaria:
+    def eliminarVista(instancia, view: ft.View) -> None:
+        """Método de eliminación de vistas"""
+        if len(instancia.page.views) > 1:
+            instancia.page.views.pop()
+            instancia.page.go( instancia.page.views[-1] )
+
+    def cambioRuta(instancia, route: ft.Page.route):
+        """Método de cambio de ruta de la página"""
+        instancia.pagina.views.clear()
+        match instancia.pagina.route:
+            case "/":
+                instancia.pagina.views.append( principal.principal(instancia.pagina) )
+        instancia.pagina.update()
+
+    def cambiarTema(instancia) -> ft.ThemeMode:
+        """Método de Personalización a la página"""
+        match instancia.pagina.client_storage.get("tema"):
+            case "claro":
+                return ft.ThemeMode.LIGHT
+            case "oscuro":
                 return ft.ThemeMode.DARK
-        else:
-            match tema:
-                case "claro":
-                    return ft.ThemeMode.LIGHT
-                case "oscuro":
-                    return ft.ThemeMode.DARK
-                case "sistema":
-                    return page.platform_brightness
+            case "sistema" | None:
+                return ft.ThemeMode.SYSTEM
 
-    page.title = "Skell's Maretu Lite"
-    page.fonts = {"Comfortaa": "fonts/comfortaa.ttf"}
-    page.theme_mode = cambiarTema()
-    page.theme = ft.Theme(
-        color_scheme_seed = cambiarColor(),
-        font_family = "Comfortaa",
-        use_material3 = True
+    def config(instancia) -> None:
+        """Método de Personalización a la página"""
+        instancia.pagina.title      = instancia.titulo
+        instancia.pagina.theme      = instancia.paleta
+        instancia.pagina.on_route_change = instancia.cambioRuta
+        instancia.pagina.on_view_pop = instancia.eliminarVista
+        instancia.pagina.go("/")
+        # instancia.pagina.update() # Actualizar Cambios
+
+    def start(instancia, page: ft.Page) -> None:
+        """Método de Inicio de Ejecución"""
+        instancia.pagina: ft.Page = page
+        instancia.config()
+
+    def __init__(instancia, titulo: str, tipografia: dict[str: str], paleta: ft.Theme) -> None:
+        """Método Constructor"""
+        instancia.titulo: str                = titulo
+        instancia.tipografia: dict[str: str] = tipografia
+        instancia.paleta: ft.Theme           = paleta
+
+# Proceso Principal
+if __name__ == "__main__":
+    # Ajustes
+    titulo: str                = "Skell's Maretu Lite"
+    tipografia: dict[str: str] = { "Comfortaa": "fonts/Comfortaa.ttf" }
+    paleta: ft.Theme           = ft.Theme(
+        color_scheme_seed      = ft.Colors.GREEN,
+        font_family            = "Comfortaa",
+        use_material3          = True
     )
-
-    def cambioDeRuta(route) -> None:
-        page.views.clear()
-        if page.route == "/":
-            page.views.append(principal(page))
-        elif page.route == "/ventas":
-            page.views.append(ventas(page))
-        elif page.route == "/reportes":
-            page.views.append(reportes(page))
-        elif page.route == "/inventario":
-            page.views.append(inventario(page))
-        elif page.route == "/clientes":
-            page.views.append(clientes(page))
-        elif page.route == "/configuracion":
-            page.views.append(configuracion(page))
-        page.update()
-    def eliminarVista(view) -> None:
-        if len(page.views) > 1:
-            page.views.pop()
-            top_view = page.views[-1]
-            page.go(top_view.route)
-
-    page.on_route_change = cambioDeRuta
-    page.on_view_pop = eliminarVista
-
-    page.go(page.route)
-    page.update()
-
-ft.app(
-    target = main,
-    assets_dir = "assets"
-)
+    # Aplicacion Primaria
+    aplicacion: Primaria = Primaria(
+        titulo     = titulo,
+        tipografia = tipografia,
+        paleta     = paleta
+    )
+    ft.app(
+        target     = aplicacion.start,
+        assets_dir = "assets"
+    )
